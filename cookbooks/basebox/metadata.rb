8dc7853618822e913c6ce6a8f18a9cbff269f39b
Richard Jones <rj@metabrew.com>
2013-12-06 13:54:39 +0000

    reset basebox ver

--- cookbooks/basebox/metadata.rb
+++ cookbooks/basebox/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures basebox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.2.4"
+version "1.0.0"
 depends          "apt"
 depends          "afw"
 #depends          "chef-client"
